feba9d5e4fb4c5682fc5aa5b74222847f8f59abc
Kaden Chow <kaden.chow@gmail.com>
2026-01-11 23:33:35 -0800

    1/11 - Initial Build, Missing Vendor Dependancies

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Code-2026Prealpha";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "ec66f5f5f60a59853f5610c0e8874566007024cf";
+  public static final String GIT_DATE = "2025-12-31 15:08:43 EST";
+  public static final String GIT_BRANCH = "annotations/Finn";
+  public static final String BUILD_DATE = "2026-01-01 03:33:30 EST";
+  public static final long BUILD_UNIX_TIME = 1767256410087L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
