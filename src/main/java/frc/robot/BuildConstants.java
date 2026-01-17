364ae59c809ccecec0089ce43760a46e7066e82c
Kaden Chow <kaden.chow@gmail.com>
2026-01-16 17:00:04 -0800

    1/16 - WIP 2026 Vision Testing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "4e95cfa979a648698bdeb848110ee73a36321c59";
-  public static final String GIT_DATE = "2026-01-15 14:35:09 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "ae13b8f9e70ebc9107b38ce1db512c0f06209309";
+  public static final String GIT_DATE = "2026-01-16 13:43:31 EST";
   public static final String GIT_BRANCH = "test/2026/vision";
-  public static final String BUILD_DATE = "2026-01-16 13:40:09 EST";
-  public static final long BUILD_UNIX_TIME = 1768588809506L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-16 17:39:53 EST";
+  public static final long BUILD_UNIX_TIME = 1768603193572L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
---
+++ src/main/java/frc/robot/subsystems/vision/TestingVision.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.vision;
+
+import frc.robot.util.VirtualSubsystem;
+
+public class TestingVision extends VirtualSubsystem {
+    
+    @Override
+    public void periodic() {}
+}
