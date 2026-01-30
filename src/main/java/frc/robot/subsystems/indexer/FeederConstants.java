9a297079ea52fc1214103fd5a036c6830dfb764e
Yo Omura <omura.yo01@gmail.com>
2026-01-29 16:56:45 -0800

    1/26 - Created IOs for the indexer subsystem

---
+++ src/main/java/frc/robot/subsystems/indexer/FeederConstants.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.indexer;
+
+public class FeederConstants {
+  public static final int feederCanId = 0;
+
+  public static final boolean feederInverted = false;
+  public static final int feederCurrentLimit = 20;
+
+  public static final double feederDebounceTime = .5;
+}
