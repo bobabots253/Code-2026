4e5a16b9b929bc5576d535913afc3b5a5fe7033d
Kaden Chow <kaden.chow@gmail.com>
2026-02-03 23:01:10 -0800

    2/3 - Build Constants, Bruh

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "05faa23da06daf07b4143972e347a2ee92942bb9";
-  public static final String GIT_DATE = "2026-02-03 00:20:39 EST";
+  public static final int GIT_REVISION = 50;
+  public static final String GIT_SHA = "2473359d0182bb529d8c4f549f8a265eab362f22";
+  public static final String GIT_DATE = "2026-02-04 01:59:44 EST";
   public static final String GIT_BRANCH = "test/2026/VisionV2";
-  public static final String BUILD_DATE = "2026-02-03 18:11:13 EST";
-  public static final long BUILD_UNIX_TIME = 1770160273941L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-04 02:00:36 EST";
+  public static final long BUILD_UNIX_TIME = 1770188436282L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
