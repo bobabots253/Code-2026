3ee506eb2758d8581eb59879badcb1a0f6b6a547
ClosetCryptid <matt.tayw774@gmail.com>
2026-02-11 15:24:44 -0800

    Update IndexerSubsystem.java

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.indexer;
 
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
 public class IndexerSubsystem extends SubsystemBase {
   private final AggitatorIO aggitatorio;
@@ -15,8 +17,18 @@ public class IndexerSubsystem extends SubsystemBase {
   public IndexerSubsystem(FeederIO feederio, AggitatorIO aggitatorio) {
     this.feederio = feederio;
     this.aggitatorio = aggitatorio;
-    agitatorDisconnectedAlert =
-        new Alert("Disconnected aggitator motor in indexter", AlertType.kError);
-    feederDisconnectedAlert = new Alert("Disconnected feeder motor in indexter", AlertType.kError);
+    agitatorDisconnectedAlert = new Alert("Disconnected Aggitator motor in indexter", AlertType.kError);
+    feederDisconnectedAlert = new Alert("Disconnected Feeder motor in indexter", AlertType.kError);
   }
+    @Override
+  public void periodic() {
+    feederio.updateInputs(feederInputs);
+    aggitatorio.updateInputs(aggitatorInputs);
+    Logger.processInputs("Aggitator", aggitatorInputs);
+    Logger.processInputs("Feeder", feederInputs);
+    agitatorDisconnectedAlert.set(!aggitatorInputs.AggitatorConnected);
+    feederDisconnectedAlert.set(!feederInputs.feederConnected);
+  }
+public Command runFeeder;
+  
 }
