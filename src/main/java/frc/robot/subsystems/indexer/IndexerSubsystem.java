1723659ac9ca66bcc2a383b26423430434776691
ClosetCryptid <matt.tayw774@gmail.com>
2026-02-11 16:48:05 -0800

    Added hasFuel method to Indexer Subsystem

--- src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSubsystem.java
@@ -5,15 +5,17 @@ import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.wpilibj.DigitalInput;
 
-public class IndexerSubsystem extends SubsystemBase {
+  public class IndexerSubsystem extends SubsystemBase {
   private final AggitatorIO aggitatorio;
   private final FeederIO feederio;
   private final Alert agitatorDisconnectedAlert;
   private final Alert feederDisconnectedAlert;
   private final AggitatorIOInputsAutoLogged aggitatorInputs = new AggitatorIOInputsAutoLogged();
   private final FeederIOInputsAutoLogged feederInputs = new FeederIOInputsAutoLogged();
-
+  private final DigitalInput beamBreakSensor = new DigitalInput(0);
+  
   public IndexerSubsystem(FeederIO feederio, AggitatorIO aggitatorio) {
     this.feederio = feederio;
     this.aggitatorio = aggitatorio;
@@ -22,6 +24,7 @@ public class IndexerSubsystem extends SubsystemBase {
   }
     @Override
   public void periodic() {
+    hasFuel();
     feederio.updateInputs(feederInputs);
     aggitatorio.updateInputs(aggitatorInputs);
     Logger.processInputs("Aggitator", aggitatorInputs);
@@ -29,6 +32,15 @@ public class IndexerSubsystem extends SubsystemBase {
     agitatorDisconnectedAlert.set(!aggitatorInputs.AggitatorConnected);
     feederDisconnectedAlert.set(!feederInputs.feederConnected);
   }
-public Command runFeeder;
-  
+  public boolean hasFuel(){
+    return beamBreakSensor.get();
+  }
+  public Command runFeeder;{
+    if (hasFuel()){
+      
+    }
+    else{
+      System.out.println("No Fuel in Feeder");    
+    }
+  }
 }
