02c236a3f5418d25aa3e4a41591680d56c31deff
Rabbid59 <130868896+Rabbid59@users.noreply.github.com>
2026-02-12 16:47:26 -0800

    2/12 - Set up basic Flywheel Subsystem

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 27;
-  public static final String GIT_SHA = "66523dc2bb8fc6c4ac94d3d3b03e5e798c111d7c";
-  public static final String GIT_DATE = "2026-02-11 21:07:33 EST";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "aec1264de664e333c2f95f855fa610ffe182bf5e";
+  public static final String GIT_DATE = "2026-02-12 19:33:15 EST";
   public static final String GIT_BRANCH = "test/2026/Finn&TheoShooterCode";
-  public static final String BUILD_DATE = "2026-02-12 19:22:07 EST";
-  public static final long BUILD_UNIX_TIME = 1770942127336L;
+  public static final String BUILD_DATE = "2026-02-12 19:39:43 EST";
+  public static final long BUILD_UNIX_TIME = 1770943183529L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/flywheel/FlywheelSubsystem.java
@@ -1,3 +1,46 @@
 package frc.robot.subsystems.shooter.flywheel;
 
-public class FlywheelSubsystem {}
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import frc.robot.fieldSetup;
+import frc.robot.subsystems.shooter.flywheel.FlywheelIO.FlywheelIOOutputs;
+import frc.robot.util.FullSubsystem;
+
+import org.littletonrobotics.junction.Logger;
+
+public class FlywheelSubsystem extends FullSubsystem {
+  private final FlywheelIO io;
+  private final Alert flywheelMasterDisconnectedAlert;
+  private final Alert flywheelFollowerDisconnectedAlert;
+  private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
+  private final FlywheelIOOutputs outputs = new FlywheelIOOutputs();
+  private final Debouncer flywheelDebouncer = new Debouncer(0.2,DebounceType.kFalling);
+
+  public FlywheelSubsystem(FlywheelIO io) {
+    this.io = io;
+    flywheelMasterDisconnectedAlert = new Alert("Disconnected Master motor in flywheel", AlertType.kError);
+    flywheelFollowerDisconnectedAlert = new Alert("Disconnected Follower motor in flywheel", AlertType.kError);
+
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Flywheel / Inputs", inputs);
+    flywheelMasterDisconnectedAlert.set(
+    Robot.showHardwareAlerts() && flywheelDebouncer.calculate(inputs.flywheelMasterConnected));
+    flywheelFollowerDisconnectedAlert.set(
+    Robot.showHardwareAlerts() && flywheelDebouncer.calculate(inputs.flywheelFollowerConnected));
+  }
+
+  @Override
+  public void periodicAfterScheduler(){
+    Logger.recordOutput("Flywheel / Outputs", outputs.mode);
+    io.applyOutputs(outputs);
+
+  }
+}
