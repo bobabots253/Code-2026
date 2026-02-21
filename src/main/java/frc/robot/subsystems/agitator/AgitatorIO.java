816338937ce3b8c1c0618eba5f6e6f07e0f5cca3
ClosetCryptid <matt.tayw774@gmail.com>
2026-02-20 16:46:15 -0800

    Finished code on AgitatorIO and IOSpark

--- src/main/java/frc/robot/subsystems/agitator/AgitatorIO.java
+++ src/main/java/frc/robot/subsystems/agitator/AgitatorIO.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems.agitator;
 
-public class AgitatorIO {}
+import org.littletonrobotics.junction.AutoLog;
+
+public interface AgitatorIO {
+  @AutoLog
+  public static class AgitatorInputs {
+    public boolean masterMotorConnected = true;
+    public double masterAppliedVolts = 0.00;
+    public double masterSupplyCurrentAmps = 0.00;
+  }
+
+  public static enum AgitatiorIOOutputMode {
+    BRAKE,
+    VOLTAGE
+  }
+
+  public static class AgitatorIOOutputs {
+    public double voltage = 0.0;
+    public AgitatiorIOOutputMode mode = AgitatiorIOOutputMode.BRAKE;
+  }
+
+  public default void updateInputs(AgitatorInputs inputs) {}
+
+  public default void applyOutputs(AgitatorIOOutputs outputs) {}
+}
