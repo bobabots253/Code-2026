e8b18307f50e3e95e809fc499f10c95708965171
Kaden Chow <kaden.chow@gmail.com>
2026-02-23 21:40:02 -0800

    2/23 - Lowered Bumper Intake Setpoint

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 44;
-  public static final String GIT_SHA = "6b8b6cc8ec5f0a4af7e80cfd41abe35f0b822638";
-  public static final String GIT_DATE = "2026-02-23 16:58:32 EST";
+  public static final int GIT_REVISION = 45;
+  public static final String GIT_SHA = "d42e69f1519462cee545179cee348b13fee94e82";
+  public static final String GIT_DATE = "2026-02-23 22:30:14 EST";
   public static final String GIT_BRANCH = "test/2026/FullRobotV1";
-  public static final String BUILD_DATE = "2026-02-23 22:28:23 EST";
-  public static final long BUILD_UNIX_TIME = 1771903703314L;
+  public static final String BUILD_DATE = "2026-02-23 22:34:30 EST";
+  public static final long BUILD_UNIX_TIME = 1771904070589L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
