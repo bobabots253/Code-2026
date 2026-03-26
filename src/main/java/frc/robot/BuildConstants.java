8f2c2e35c1713ff148b1d736f447b7f11c93c296
Kaden Chow <kaden.chow@gmail.com>
2026-03-26 01:02:04 -0700

    3/26 - Rm Field Constants to prevent AprilTagFieldLayout JSON fetching as a possible source of lag

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 126;
-  public static final String GIT_SHA = "ba3356cd0544b1b6401bd529d24eed067fa4a076";
-  public static final String GIT_DATE = "2026-03-23 03:39:08 EDT";
+  public static final int GIT_REVISION = 127;
+  public static final String GIT_SHA = "5499e3b4bcb361ac72222cddb68aa0bb577a3382";
+  public static final String GIT_DATE = "2026-03-26 02:44:05 EDT";
   public static final String GIT_BRANCH = "test/2026/FullRobotV3";
-  public static final String BUILD_DATE = "2026-03-26 02:41:37 EDT";
-  public static final long BUILD_UNIX_TIME = 1774507297587L;
+  public static final String BUILD_DATE = "2026-03-26 04:00:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1774512005813L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
