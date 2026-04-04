48c4ee13ed208a8c80fe42cc093bd4bec816d96d
Kaden Chow <kaden.chow@gmail.com>
2026-04-04 07:10:36 -0700

    4/4 - Right Autos to Showcase Capabilities

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 139;
-  public static final String GIT_SHA = "4283c5b58b60ba89ae2fbb5b7f0789e4eb650762";
-  public static final String GIT_DATE = "2026-04-03 01:59:16 EDT";
+  public static final int GIT_REVISION = 140;
+  public static final String GIT_SHA = "3d71e87bfd9e5235a2e4f2ca20530efc6ac3a736";
+  public static final String GIT_DATE = "2026-04-03 21:41:44 EDT";
   public static final String GIT_BRANCH = "test/2026/FullRobotV3";
-  public static final String BUILD_DATE = "2026-04-03 21:40:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1775266852321L;
+  public static final String BUILD_DATE = "2026-04-04 10:09:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1775311762065L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
