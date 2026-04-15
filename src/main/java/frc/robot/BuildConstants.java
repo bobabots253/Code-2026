db029cfcf1197c9592db3ccb929bbd2e2da3e3b1
Rabbid59 <130868896+Rabbid59@users.noreply.github.com>
2026-04-14 19:23:11 -0700

    4/14 Updated BuildConstants fix?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Code-2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 144;
-  public static final String GIT_SHA = "ed5c4acb30be119ccf1ebee35dda84bab265c54b";
-  public static final String GIT_DATE = "2026-04-07 02:03:10 EDT";
-  public static final String GIT_BRANCH = "test/2026/FinnTest";
-  public static final String BUILD_DATE = "2026-04-07 19:59:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1775606354747L;
+  public static final int GIT_REVISION = 158;
+  public static final String GIT_SHA = "b6c923628e233616407657cfb9223cd528923c30";
+  public static final String GIT_DATE = "2026-04-13 01:23:41 EDT";
+  public static final String GIT_BRANCH = "test/2026/FullRobotV3";
+  public static final String BUILD_DATE = "2026-04-13 23:35:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1776137741238L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
